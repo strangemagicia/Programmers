b3ce8905bf62476decae9eb6f3e950d1e7d00355
김민경 <100396334+strangemagicia@users.noreply.github.com>
2025-01-19 12:47:08 +0900

    문자열

--- Solution1.java
+++ Solution1.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Solution1{
   public static void main(String[] args) {
     String message = "Let's go";
+    
     System.out.println("3\n2\n1");
     System.out.println(message);
   }
