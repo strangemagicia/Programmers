f0b1b27c453c106e442e037e8684fb17d18006b8
김민경 <100396334+strangemagicia@users.noreply.github.com>
2025-01-19 12:46:44 +0900

    정수 a, b 출력

--- Solution2.java
+++ Solution2.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Solution2 {
   public static void main(String args[]){
     Scanner sc = new Scanner(System.in);
+    
     int a = sc.nextInt();
     int b = sc.nextInt();
 
